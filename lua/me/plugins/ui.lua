97073392b4fe0316f03d885c5d9c3783fc3fc76b
Fabio Lusha <fabio.lusha@mail.polimi.it>
2026-02-19 22:44:20 +0100

    Move indentation rails from editor to ui

--- lua/me/plugins/ui.lua
+++ lua/me/plugins/ui.lua
@@ -1,1 +1,19 @@
-return {}
+return {
+  -- indentation guidelines/rails
+  -- {
+  --   'lukas-reineke/indent-blankline.nvim',
+  --   main = 'ibl',
+  --   ---@module 'ibl'
+  --   ---@type ibl.config
+  --   opts = {
+  --     indent = {
+  --       char = '│',
+  --     },
+  --     -- show_current_context = true,
+  --     -- show_current_context_start = true,
+  --     -- use_treesitter = true,
+  --     -- char_highlight_list = { 'IndentBlanklineChar' },
+  --     -- context_char_highlight_list = { 'IndentBlanklineContextChar' },
+  --   },
+  -- },
+}
